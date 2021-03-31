9303a661b62411c56205a37f63bd3e4c83c779f6
shk3029 <shk30290@naver.com>
2021-03-31 13:35:12 +0900

    feat: Flux Study Test

---
+++ src/test/java/me/js/async/study/FluxStudyTest.java
@@ -0,0 +1,39 @@
+package me.js.async.study;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class FluxStudyTest {
+
+    @Test
+    void createAFlux_just() {
+        Flux<String> flux = Flux.just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+        flux.subscribe(f -> log.info("Here's some fruit :  " + f)); // subscribe를 호출하는 즉시 데이터가 전달되기 시작
+
+        StepVerifier.create(flux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    void createAtFlux_fromArray() {
+        String[] fruits = new String[] {"Apple", "Orange", "Grape", "Banana", "Strawberry"};
+
+        Flux<String> flux = Flux.fromArray(fruits);
+
+        StepVerifier.create(flux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+}
